6a361831403cb6066a4965a59a0877001a684418
kfleung129 <endyjjhk@gmail.com>
2024-05-16 19:20:54 +0800

    added item hover styling

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -5,7 +5,7 @@ export default function CategoryItem(props) {
   const { src, name, price } = props;
   return (
     <Grid item xs={6} md={4} textAlign='center'>
-      <Box display='flex' flexDirection='column' alignItems='center'>
+      <Box display='flex' flexDirection='column' alignItems='center' sx={{ cursor: 'pointer', transition: '0.2s', '&:hover': { transform: 'scale(1.1)' } }}>
         <Box sx={{ width: { xs: 150, md: 300 }, height: { xs: 150, md: 300 } }}>
           <div style={{ width: '100%', height: '100%', position: 'relative'}}>
             <Image
